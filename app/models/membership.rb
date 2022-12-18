d63c12a38a1c88188c306e875a8b3bb59bd84ad6
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-12-18 16:36:53 -0500

    commit tree a787fb68c0dc0e551a28b1edf0af41a33b9b857f

---
+++ app/models/membership.rb
@@ -0,0 +1,8 @@
+class Membership < ApplicationRecord
+  belongs_to :gym
+  belongs_to :client
+
+  validates :gym_id, :client_id, :charge, presence: true
+  validates :client_id, uniqueness: true
+
+end
